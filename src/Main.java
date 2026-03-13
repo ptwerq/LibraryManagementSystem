f4883cb3bf6f7018d0754a713d5a5407372d77d8
Alexander Medvedev <its.alexander.medvedev@gmail.com>
2026-03-13 15:17:15 +0100

    Refactor main method to be public and properly formatted; improve code readability

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 import model.Book;
 import model.Genre;
 
-void main() {
-    Scanner scanner = new Scanner(System.in);
-    String title = scanner.nextLine();
-    int year = scanner.nextInt();
-    scanner.nextLine();
-    Genre genre = Genre.valueOf(scanner.nextLine().toUpperCase());
-    String author = scanner.nextLine();
-    int pages = scanner.nextInt();
-    scanner.nextLine();
+import java.util.Scanner;
 
-    Book book = new Book(title, year, genre, author, pages);
-    Book book2 = new Book(title, year, genre, author, pages);
-    System.out.println(book.getId());
-    System.out.println(book2.getId());
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        int year = scanner.nextInt();
+        scanner.nextLine();
+        Genre genre = Genre.valueOf(scanner.nextLine().toUpperCase());
+        String author = scanner.nextLine();
+        int pages = scanner.nextInt();
+        scanner.nextLine();
+
+        Book book = new Book(title, year, genre, author, pages);
+        Book book2 = new Book(title, year, genre, author, pages);
+        System.out.println(book.getId());
+        System.out.println(book2.getId());
+    }
 }
 
 
